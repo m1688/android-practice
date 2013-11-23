cebe4880062abee68053a5abaf6b7fdca9cc58d8
jyu <wee.jud@gmail.com>
2013-11-23 23:28:06 +0800

    make the standard packages

---
+++ src/main/java/m/infrastructure/MemRepository.java
@@ -0,0 +1,9 @@
+package m.infrastructure;
+
+/**
+ * read entries from memory
+ *
+ * Created by wee on 11/23/13.
+ */
+public abstract class MemRepository {
+}
---
+++ src/main/java/m/infrastructure/impl/DefaultUserRepository.java
@@ -0,0 +1,12 @@
+package m.infrastructure.impl;
+
+import m.domain.user.UserRepository;
+import m.infrastructure.MemRepository;
+
+/**
+ * impl of user repository
+ *
+ * Created by wee on 11/23/13.
+ */
+public class DefaultUserRepository extends MemRepository implements UserRepository {
+}
