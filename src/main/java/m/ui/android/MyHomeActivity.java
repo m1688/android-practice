fea6698e4e9b13b1dc23b42c25a2de727e585686
chunterg <chunterg@gmail.com>
2013-12-05 10:28:56 +0800

    增加滑动删除，需要依赖类库drag-sort-view

--- src/main/java/m/ui/android/MyHomeActivity.java
+++ src/main/java/m/ui/android/MyHomeActivity.java
@@ -2,15 +2,20 @@ package m.ui.android;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AbsListView;
 import android.widget.AnalogClock;
@@ -22,6 +27,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+import com.mobeta.android.dslv.DragSortListView;
+import com.mobeta.android.dslv.DragSortController;
 
 import m.domain.tweet.Tweet;
 import m.domain.user.User;
@@ -39,26 +49,24 @@ public class MyHomeActivity extends Activity {
 //    private static final int pageSize = 10;
 //    private int pageNum = 1;
 //    private int lastUserIndex = 0;
-    
+    private GestureDetector mGesture;
+    TweetListAdapter adapter;
     private List<Tweet> TweetData = SampleData.twList;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_myhome);
-        
-        ListView lv = (ListView)findViewById(R.id.my_timeline);
-        Log.d("App-twitter","ListView created");
-        TweetListAdapter adapter = new TweetListAdapter(this,R.layout.tweet_item,TweetData);
-        Log.d("App-twitter","Adapter created");
-        lv.setAdapter(adapter);
-//        ListView userListView = (ListView)findViewById(R.id.userListView);
-//        UserListAdapter userListAdapter = new UserListAdapter(this, android.R.layout.simple_list_item_2, SampleData.list);
-//        userListView.setAdapter(userListAdapter);
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		  super.onCreate(savedInstanceState);
+		  setContentView(R.layout.activity_myhome);
+		  
+		  DragSortListView lv = (DragSortListView) findViewById(R.id.my_timeline);
+	      lv.setDragSortListener(onGoing);
+//	      lv.setRemoveListener(onRemove);
 
-
+		  adapter = new TweetListAdapter(this,R.layout.tweet_item,TweetData);
+		  Log.d("App-twitter","Adapter created");
+		  lv.setAdapter(adapter);
+	}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -81,4 +89,38 @@ public class MyHomeActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+        private DragSortListView.RemoveListener onRemove = 
+            new DragSortListView.RemoveListener() {
+                @Override
+                public void remove(int which) {
+//                	remove callback
+                    Log.d("App-twitter", "delete:"+TweetData.get(which).getTid());
+                    adapter.remove(adapter.getItem(which));
+                }
+            };
+            
+        private DragSortListView.DragSortListener onGoing =
+        		new DragSortListView.DragSortListener() {
+					
+					@Override
+					public void remove(int which) {
+						// TODO Auto-generated method stub
+						Log.d("App-twitter", "delete:"+TweetData.get(which).getTid());
+//	                    adapter.remove(adapter.getItem(which));
+					}
+					
+					@Override
+					public void drag(int from, int to) {
+						// TODO Auto-generated method stub
+						Log.d("App-twitter", "drag from:"+from+" to:"+to);
+					}
+					
+					@Override
+					public void drop(int from, int to) {
+						// TODO Auto-generated method stub
+						Log.d("App-twitter", "drop from:"+from+" to:"+to);
+						
+					}
+				};
 }
--- src/main/java/m/ui/android/adapter/TweetListAdapter.java
+++ src/main/java/m/ui/android/adapter/TweetListAdapter.java
@@ -2,6 +2,9 @@ package m.ui.android.adapter;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,6 +14,7 @@ import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -26,7 +30,10 @@ public class TweetListAdapter extends ArrayAdapter<Tweet> {
 
     private final Context context;   
     private List<Tweet> tweetList;
-    private int resource;
+    private int resource;  
+    int startX = 0;
+    int endX = 0;
+    private GestureDetector mGesture = null;
 
     public TweetListAdapter(Context context, int resource, List<Tweet> tweetList) {
 
@@ -40,14 +47,16 @@ public class TweetListAdapter extends ArrayAdapter<Tweet> {
         public TextView twitterUserName;     
         public TextView twitterContent; 
      }
+
     
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
     	ListItemView  listItemView = null;
+    	
     	if (convertView == null) {
     		listItemView = new ListItemView();
 //    		获取布局直接调用，不用实例化
-            convertView = LayoutInflater.from(context).inflate(R.layout.tweet_item, null);
+            convertView = LayoutInflater.from(context).inflate(resource, null);
             listItemView.twitterUserName = (TextView) convertView.findViewById(R.id.twitter_username);
             listItemView.twitterContent = (TextView) convertView.findViewById(R.id.twitter_content);
             convertView.setTag(listItemView); 
@@ -55,18 +64,36 @@ public class TweetListAdapter extends ArrayAdapter<Tweet> {
         }else {   
             listItemView = (ListItemView)convertView.getTag();   
         }
+    	
 		// 为每一个view项设置触控监听
-    	convertView.setOnTouchListener(new OnTouchListener() {
-
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-//				Listview touch事件
-				Log.d("app-msg",event.toString());
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-		});  
+//    	convertView.setOnTouchListener(new OnTouchListener() {
+//    		  
+//			@Override
+//			public boolean onTouch(View v, MotionEvent event) {
+////				Listview touch事件
+//				 switch(event.getAction())//根据动作来执行代码     
+//                 {    
+//                 case MotionEvent.ACTION_MOVE://滑动     
+//                     Toast.makeText(context, "move...", Toast.LENGTH_SHORT).show();  
+//                     break;    
+//                 case MotionEvent.ACTION_DOWN://按下     
+//                     startX = (int) event.getX(); 
+//                     Log.d("app-twitter", "start:"+startX+"");
+//                     break;    
+//                 case MotionEvent.ACTION_UP://松开     
+//                	 endX = (int) event.getX();  
+//                	 Log.d("app-twitter", "end:"+endX+"");
+//                	 Log.d("app-twitter", "move:"+(Math.abs(endX-startX))+"");
+//                	 if(Math.abs(endX-startX) > 20){  
+////                         holder.cBox.setVisibility(View.VISIBLE);  
+//                     }  
+//                     break;    
+//                 default:    
+//                 }    
+//                 return true;   
+//			}
+//			
+//		});  
     	listItemView.twitterUserName.setText(tweetList.get(position).getUserName());
     	listItemView.twitterContent.setText(tweetList.get(position).getContent());
         return convertView;
